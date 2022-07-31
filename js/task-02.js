1a186af3249b72bc61def9534b2a701a354807aa
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-07-31 18:47:02 +0300

    done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('ul#ingredients');
+
+const getLiMarkup = (ingredient) => `<li class="item">${ingredient}</li>`
+const getLiListMarkup = (ingredientsList) => ingredientsList.map(it => getLiMarkup(it)).join('')
+
+ingredientsRef.insertAdjacentHTML('afterbegin', getLiListMarkup(ingredients));
