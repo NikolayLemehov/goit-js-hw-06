dbbd8f1d1f7bc3f354238637e50ae675045cff60
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-07-31 22:05:22 +0300

    done task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const ref = {
+  boxes: document.querySelector('div#boxes'),
+  input: document.querySelector('input[type="number"]'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+}
+
+const getItemMarkup = (index) => `<div class="item"
+ style="width: ${30 + 10 * index}px; height: ${30 + 10 * index}px; background-color: ${getRandomHexColor()}"></div>`
+
+const getItemListMarkup = (amount) => Array(amount).fill('').map((_, i) => getItemMarkup(i)).join('')
+
+ref.createBtn.addEventListener('click', handleClickCreate);
+ref.destroyBtn.addEventListener('click', handleClickDestroy);
+
+
+function handleClickCreate() {
+  clearBoxes();
+  createBoxes(+ref.input.value)
+}
+
+function handleClickDestroy() {
+  clearBoxes();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  ref.boxes.insertAdjacentHTML('afterbegin', getItemListMarkup(amount));
+}
+
+function clearBoxes() {
+  while (ref.boxes.childNodes.length > 0) {
+    ref.boxes.firstChild.remove();
+  }
 }
