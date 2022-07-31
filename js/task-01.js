1d6136526426c9e4bfa53bf13722273ac918d93a
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-07-31 18:23:58 +0300

    done task-01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const categoriesListRef = document.querySelector('ul#categories');
+const categoryItems = categoriesListRef?.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+categoryItems.forEach(it => {
+  const category = it.querySelector('h2').textContent;
+  const elementsCount = it.querySelectorAll(':scope > ul > li').length;
+  console.log(`Category: ${category}\nElements: ${elementsCount}`)
+})
